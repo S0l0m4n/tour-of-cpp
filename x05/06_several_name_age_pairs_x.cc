55c5cadc07c55d2e2e5ccebfc51384b2821715d9
S0l0m4n <sam@stickerbrush>
2023-12-26 12:50:07 +0200

    Add alternative version of X3-6 for experimentation

---
+++ x05/06_several_name_age_pairs_x.cc
@@ -0,0 +1,70 @@
+/**
+ * Read several name/age pairs into a class, defining my own `<<` and `>>`
+ * operators.
+ */
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::string;
+using std::pair;
+using std::vector;
+
+class NameAgePairs {
+public:
+    // Constructor
+    NameAgePairs(int max) : elems {new string[max]}, sz {max} {}
+
+    bool add_pair(string na_pair) {
+        if (count < sz) {
+            elems[count] = na_pair;
+            count++;
+            return true;
+        }
+        return false;
+    }
+
+    std::string get_name(int i) {
+        if (i < count)
+            return elems[i];
+        return "NULL";
+    }
+
+    int get_size() {
+        return count;
+    }
+
+private:
+    string* elems;
+    int sz;
+    int count = 0;
+};
+
+// Overload the `>>` operator
+std::istream& operator>>(std::istream& input, NameAgePairs& na_pairs) {
+    string na_pair;
+    if (input >> na_pair)
+        na_pairs.add_pair(na_pair);
+    return input;
+}
+
+int main()
+{
+    NameAgePairs pairs(8);
+
+    std::cout << "Please enter several name and age pairs (hit `Enter` after each pair), e.g. 'John 44': "
+              << std::endl;
+
+    while (std::cin >> pairs) {
+        // Do nothing
+    }
+
+    // Continue processing from here
+    for (auto i = 0; i < pairs.get_size(); i++) {
+        std::cout << pairs.get_name(i) << std::endl;
+    }
+
+    return 0;
+}
